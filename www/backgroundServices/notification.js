13a29fd646dc097ff55bd2ac7c8eac2e037d8ea9
raphael.weaver <email@irahtech.com>
2016-03-25 23:33:38 -0400

    GCA -8 #resolve

--- www/backgroundServices/notification.js
+++ www/backgroundServices/notification.js
@@ -1,3 +1,34 @@
-/**
- * Created by Raphael on 3/25/2016.
- */
+var gcalarm = angular.module('gcalarm', []);
+
+//Model
+var NotificationModel = (function(){
+  function Cow(){
+    this.ateGrass = 0;
+  }
+  NotificationModel.prototype.eatGrass = function(){
+    var isAlive = false;
+    if(this.ateGrass < 4){
+      this.ateGrass += 1;
+      isAlive = true;
+    }
+    return isAlive;
+  }
+  return NotificationModel;
+})();
+
+//Module(Service)
+gcalarm.service('notification', function() {
+  this.ateGrassTotal = 0;
+  this.evalFarm = function(times,cows) {
+    var grass = times - this.ateGrassTotal;
+    var newCows = [];
+    for(var i = 0;i < cows.length; i++){
+      if(cows[i].eatGrass()){
+        grass -= 1;
+        this.ateGrassTotal += 1;
+        newCows.push(cows[i]);
+      }
+    }
+    return {grass:grass,cows:newCows}
+  };
+});
